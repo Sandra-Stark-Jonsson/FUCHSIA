68de102de4dcea6737032478ffc42877865f5fc9
Sandra Stark Jönsson <jonssonsandra98@hotmail.com>
2021-05-11 16:37:58 +0200

    Pillat på js filen
    Co-Authored-By: EllinorKortz <82371026+EllinorKortz@users.noreply.github.com>
    Co-Authored-By: melissa4-b <82364118+melissa4-b@users.noreply.github.com>

--- Kodstruktur/js_files/destinations.js
+++ Kodstruktur/js_files/destinations.js
@@ -12,9 +12,33 @@ let database = {
     baseArray: CITIES,
     filterKey: "name",
     filterLabelKey:"",
+    DOMcreater: DOMcities
 };
 
 console.log(database);
+
+function DOMcities (cities){
+    let contaier = document.createElement ("div");
+    contaier.classList.add("cities");
+
+    contaier.append(cityName());
+
+    contaier.append(cityInfo());
+
+//Denna funktion fungerar inte men vi tror att vi är på rätt spår    
+
+    function cityName(){
+
+        let cityDiv = document.createElement("div");
+
+        cityDiv.innerHTML = `<div>${cities.name}</div>`;
+
+        console.log(cityDiv);
+
+        return cityDiv;
+    };
+
+};
 
 function getCountry(database) {
 
@@ -27,6 +51,8 @@ function getCountry(database) {
     input.addEventListener("keyup", function() {
 
         document.querySelector("")
+
+
 
     });
 };
