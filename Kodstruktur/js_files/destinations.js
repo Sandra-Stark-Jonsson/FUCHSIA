47ddf90368c0ab6b3e0c9c1686fedfe78963896a
Sandra Stark Jönsson <jonssonsandra98@hotmail.com>
2021-05-17 12:51:23 +0200

    försökt få in universiteten
    Co-Authored-By: EllinorKortz <82371026+EllinorKortz@users.noreply.github.com>
    Co-Authored-By: melissa4-b <82364118+melissa4-b@users.noreply.github.com>
    Co-Authored-By: qlaramarie <82370828+qlaramarie@users.noreply.github.com>

--- Kodstruktur/js_files/destinations.js
+++ Kodstruktur/js_files/destinations.js
@@ -13,27 +13,57 @@ document.querySelector("#buttonCity").addEventListener("click", function () {
   console.log(filterCities);
   filterCities.forEach(function (city) {
     let list = document.querySelector("#listContainer");
-    list.append(DOMcities(city.imagesNormal[0], city.name, city.text));
+    list.append(DOMcities(city.imagesNormal[0], city.name, city.text, city.id));
   });
 });
 
 // console.log(COUNTRIES);
 
-function DOMcities(bild, name, text) {
+function DOMcities(bild, name, text, id) {
   let container = document.createElement("div");
   container.classList.add("cities");
 
   container.innerHTML = `
-    <img src = 'Images/${bild}'>
+  <div class="startDiv">
+    <img class="startImage" src = 'Images/${bild}'>
     <div class= "cityContainer">
         <h1>${name}</h1>
         <p>${text}</p>
         <button>Läs Mer</button>
     </div>
-`;
+  </div>  
+  <div class="extra">
+    <div class="gradeImage">
+      <div class="grades"></div>
+      <img class="extraImage" src='Images/${bild}'>
+    </div>
+    <div class="universitet">
+      ${createUniversity(id)}
+    </div>
+  </div>
+  `;
 
+  
+   
   return container;
 }
+
+function createUniversity(id){
+
+  let universityCity = UNIVERSITIES.filter(element => element.cityID === id);
+  console.log(universityCity);
+
+  universityCity.forEach(skola => {
+    
+    let universityDiv = document.createElement("div");
+    universityDiv.innerHTML = `
+
+    <h2>${skola.name}</h2>
+    
+    `
+    return universityDiv;
+  }); 
+} 
 
 // let DOMcontainer = document.createElement("container");
 
