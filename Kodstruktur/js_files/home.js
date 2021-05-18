e170ea2ef85f2c6dac231b6391099dfc8f42f293
qlaramarie <claramarielund@gmail.com>
2021-05-18 15:51:38 +0200

    karusellen funkar nästan !!

--- Kodstruktur/js_files/home.js
+++ Kodstruktur/js_files/home.js
@@ -7,29 +7,41 @@ const prevButton = document.querySelector(".button-prev");
 const nextButton = document.querySelector(".button-next");
 
 // //counter
-let counter = 1;
-const size = carouselCell[0].clientWidth;
+let counter = 0;
+const size = carouselCell[2].clientWidth;
 
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 //Button Event listners
 nextButton.addEventListener("click", function () {
-  carouselSlide.style.transition = "transform 0.4 ease-in-out";
+  //bug preventor
+  if (counter >= carouselCell.length - 1) return;
+  console.log('size' + size);
+  console.log('counter' + counter);
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter++;
-  carouselSlide.style.transform = 'translateX(" + -size * counter + "px)';
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 
 prevButton.addEventListener("click", function () {
+  //bug preventor
+  if (counter <= 0) return;
   carouselSlide.style.transition = "transform 0.4 ease-in-out";
   counter--;
-  carouselSlide.style.transform = 'translateX(" + -size * counter + "px)';
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 
-//Reset the transition between cells
-carouselSlide.addEventListener("transitionend", function () {
+//Reset the transition between cells prev button
+carouselSlide.addEventListener('transitionend', function () {
+  if (carouselCell[counter].id === "firstCell") {
+    //carouselSlide.style.transition = "none";
+    counter = carouselCell.length - 2;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
   if (carouselCell[counter].id === "sixthCell") {
-    carouselSlide.style.transition = "none";
-    counter = carouselCell.length - 1;
-    carouselSlide.style.transform = 'translateX(" + -size * counter + "px)';
+    //carouselSlide.style.transition = "none";
+    counter = carouselCell.length - counter;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
   }
 });
+
