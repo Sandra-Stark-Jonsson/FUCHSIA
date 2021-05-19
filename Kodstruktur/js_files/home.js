d6ea1ef11ccb921b51e0cf1f23b3c621ec74a274
EllinorKortz <82371026+EllinorKortz@users.noreply.github.com>
2021-05-19 12:46:11 +0200

    test

--- Kodstruktur/js_files/home.js
+++ Kodstruktur/js_files/home.js
@@ -1,5 +1,5 @@
 //karusellen
-/*const carouselSlide = document.querySelector(".carousel");
+const carouselSlide = document.querySelector(".carousel");
 const carouselCell = document.querySelectorAll(".carousel .carousel-cell");
 
 //the buttons
@@ -45,4 +45,3 @@ carouselSlide.addEventListener('transitionend', function () {
   }
 });
 
-*/
