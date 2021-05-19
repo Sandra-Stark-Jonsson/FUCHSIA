711ba0a288364b72123f2797bd4f840a0da05eb1
Sandra Stark Jönsson <jonssonsandra98@hotmail.com>
2021-05-19 13:46:09 +0200

    CSS på programsiden

--- Kodstruktur/js_files/programs.js
+++ Kodstruktur/js_files/programs.js
@@ -14,30 +14,42 @@ document.querySelector("#buttonProgram").addEventListener("click", function () {
   console.log(filterPrograms);
   filterPrograms.forEach(function (program) {
     let list = document.querySelector("#listContainer");
-    list.append(DOMprograms(program.name, program.universityID, program.level, program.language));
+    list.append(DOMprograms(program.name, program.universityID, program.level, program.language, program.entryGrades));
   
   });
 });
 
 
 
-function DOMprograms(name, universityID, level, language){
+function DOMprograms(name, universityID, level, language, entryGrades){
   let container = document.createElement("div");
   container.classList.add("program");
 
   container.innerHTML = `
   <div class="startdiv">
     <div class="programContainer">
-      <div id="rubrik"></div>
-        <hi id="programH1">${name}</h1>
-        <p id="level"> - ${level}</p>
+      <div class="allInfo">
+        <h1 id="programH1">${name} - ${level}</h1>
+        <div id="info">
+          <p class="programP">${universityID}</p>
+          <p class="programP">${language}</p>
+        </div>  
+        <div id="studentReview"></div>
       </div>
-      <div id="info">
-        <p class="programP">${universityID}</p>
-        <p class="programP">${language}</p>
-      </div>  
+      <div id="grades">
+        <p class="eg">2021</p>
+        <p class="eg">2020</p>
+        <p class="eg">2019</p>
+        <p class="eg">2018</p>
+        <p class="eg">2017</p>
+        <p class="eg">${entryGrades[0]}</p>
+        <p class="eg">${entryGrades[1]}</p>
+        <p class="eg">${entryGrades[2]}</p>
+        <p class="eg">${entryGrades[3]}</p>
+        <p class="eg">${entryGrades[4]}</p>
+      </div>
     </div>
-    <div></div>
+    <div class="students"></div>
   </div>
   `;
   return container;
