16e73f739ac512667df559aab17f72a03c5f8a08
EllinorKortz <82371026+EllinorKortz@users.noreply.github.com>
2021-05-21 09:38:38 +0200

    fixat bilden på destinations länder

--- Kodstruktur/js_files/destinations.js
+++ Kodstruktur/js_files/destinations.js
@@ -153,6 +153,12 @@ function createCities(idCountry) {
       citytext.innerHTML = city.text;
       cityelement.append(citytext);
 
+      let cityBild = document.createElement("h2");
+      cityBild.innerHTML = city.imagesNormal[0];
+      cityelement.append(cityBild);
+
+      
+
 
 
       UNIVERSITIES.forEach(function (uni) {
